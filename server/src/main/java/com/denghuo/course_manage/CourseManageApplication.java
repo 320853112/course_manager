22229ab5d1e7f9495dd48f9a149df9cc205bc616
henryXu <320853112@qq.com>
2019-12-03 16:03:48 +0800

    12.03

--- server/src/main/java/com/denghuo/course_manage/CourseManageApplication.java
+++ server/src/main/java/com/denghuo/course_manage/CourseManageApplication.java
@@ -3,16 +3,22 @@ package com.denghuo.course_manage;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.denghuo.course_manage.dao")
 @ComponentScan("springfox.documentation.swagger2")
 @ComponentScan("com.denghuo.course_manage")
-public class CourseManageApplication {
+public class CourseManageApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CourseManageApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CourseManageApplication.class);
+    }
 }

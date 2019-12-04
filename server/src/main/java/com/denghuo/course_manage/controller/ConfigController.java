0e07bfc6b52b3179b8e8c32cc162fffa392e1563
henryXu <320853112@qq.com>
2019-12-04 12:55:50 +0800

    12.04

--- server/src/main/java/com/denghuo/course_manage/controller/ConfigController.java
+++ server/src/main/java/com/denghuo/course_manage/controller/ConfigController.java
@@ -1,16 +1,22 @@
 package com.denghuo.course_manage.controller;
 
 import com.denghuo.course_manage.utils.Result;
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 import java.io.IOException;
 
 @RestController
+@Api(value = "studentAPI",tags = "配置相关")
 public class ConfigController {
-    @RequestMapping("/restartFont")
-    public Object restartFont(){
+    @RequestMapping(value = "/restartFont",method = RequestMethod.POST)
+    public Object restartFont(String key){
+        if(!key.equals("denghuo")){
+            return Result.error() ;
+        }
         ProcessBuilder pb = new ProcessBuilder("/MyShell/restart_manage_front.sh");
 
         int runningStatus = 0;

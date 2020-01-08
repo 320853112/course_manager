45bfa7b6220a4f41809c600f37ddf7817c2ac563
xuhang <320853112@qq.com>
2020-01-08 15:03:53 +0800

    返回用户名

--- server/src/main/java/com/denghuo/course_manage/service/serviceimpl/LoginServiceImpl.java
+++ server/src/main/java/com/denghuo/course_manage/service/serviceimpl/LoginServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 
 @Service
 public class LoginServiceImpl implements LoginService {
@@ -23,42 +24,52 @@ public class LoginServiceImpl implements LoginService {
     @Override
     public Boolean stuLogin(Integer stuId, String password) {
         password = MD5util.getMD5String(password);
-        if(loginDAO.stuLogin(stuId, password)==null){
+        String userName = loginDAO.stuLogin(stuId, password);
+        if(userName==null){
             throw new CustomException(MyExceptionEnum.ACCESS_FAIL);
         }
         session.setAttribute("roleNum", Role_Data.student.getRoleNum());
         session.setAttribute("roleName", Role_Data.student.getRoleName());
+        session.setAttribute("userName", userName);
         return true;
     }
 
     @Override
     public Boolean teacherLogin(Integer teacherId, String password) {
         password = MD5util.getMD5String(password);
-        if(loginDAO.teacherLogin(teacherId, password)==null){
+        String userName = loginDAO.teacherLogin(teacherId, password);
+        if(userName==null){
             throw new CustomException(MyExceptionEnum.ACCESS_FAIL);
         }
         session.setAttribute("roleNum", Role_Data.teacher.getRoleNum());
         session.setAttribute("roleName", Role_Data.teacher.getRoleName());
+        session.setAttribute("userName", userName);
         return true;
     }
 
     @Override
     public Boolean adminLogin(String username, String password) {
         password = MD5util.getMD5String(password);
-        if(loginDAO.adminLogin(username, password)==null){
+        String userName = loginDAO.adminLogin(username, password);
+        if(userName==null){
             throw new CustomException(MyExceptionEnum.ACCESS_FAIL);
         }
         session.setAttribute("roleNum", Role_Data.admin.getRoleNum());
         session.setAttribute("roleName", Role_Data.admin.getRoleName());
+        session.setAttribute("userName", userName);
         return true;
     }
 
     @Override
-    public String getUserRole(HttpSession session) {
+    public HashMap getUserRole(HttpSession session) {
         String roleName = (String)session.getAttribute("roleName");
+        String userName = (String)session.getAttribute("userName");
         if(roleName ==null) {
             throw new CustomException(MyExceptionEnum.PLEASE_LOGIN);
         }
-        return roleName;
+        HashMap resMap = new HashMap();
+        resMap.put("roleName",roleName);
+        resMap.put("userName",userName);
+        return resMap;
     }
 }

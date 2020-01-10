105231af55ceef751c20290a12bc9713deab08d0
xuhang <320853112@qq.com>
2020-01-10 15:54:33 +0800

    修改选课

--- server/src/main/java/com/denghuo/course_manage/service/serviceimpl/SelectCourseServiceImpl.java
+++ server/src/main/java/com/denghuo/course_manage/service/serviceimpl/SelectCourseServiceImpl.java
@@ -35,7 +35,7 @@ public class SelectCourseServiceImpl implements SelectCourseService {
     @Transactional
     public boolean pickCourse(String stuId, String courseId) {
         //先查询验证学生和课程是否都存在
-        List<Student> stuInfo = studentDAO.getStuInfo(new Student(stuId),1,1);
+        List<Student> stuInfo = studentDAO.getStuInfo(new Student(stuId),0,1);
         Course course = courseDAO.existCourse(courseId);
         if (stuInfo.size() != 1 || course!=null) {
             throw new CustomException(MyExceptionEnum.ACCESS_FAIL);
